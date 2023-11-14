5edfea56eef918e989311773a615c8a308331ca2
Leandro Dal Molin <1050873+leandrodalmolin@users.noreply.github.com>
2023-11-14 18:08:22 +0000

    Add zinc colours and body styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
-import styles from './page.module.scss'
-
 export default function Home() {
   return (
     <div>
-      <h1 className={styles.h1}>Portfolio</h1>
+      <h1>Portfolio</h1>
     </div>
   )
 }
