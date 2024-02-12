bbeeb1ab9791ca867cc3af2d93d41877955165b3
Leandro Dal Molin <1050873+leandrodalmolin@users.noreply.github.com>
2024-02-12 09:47:55 +0000

    Fix navbar size on component mount

--- src/hooks/useScrollStatus.ts
+++ src/hooks/useScrollStatus.ts
@@ -6,7 +6,9 @@ export function useScrollStatus(threshold = 300) {
   useEffect(() => {
     const handleScroll = () => setIsScrolling(window.scrollY > threshold)
 
+    handleScroll() // Guarantee right status on component mount
     window.addEventListener('scroll', handleScroll)
+
     return () => window.removeEventListener('scroll', handleScroll)
   }, [threshold])
 
