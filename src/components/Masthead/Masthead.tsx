12b025fe59ee5a3134dc119d0de665227a0e7b97
Leandro Dal Molin <1050873+leandrodalmolin@users.noreply.github.com>
2024-04-08 20:34:12 +0100

    Add noise and scramble effect to masthead

--- src/components/Masthead/Masthead.tsx
+++ src/components/Masthead/Masthead.tsx
@@ -1,14 +1,30 @@
+'use client'
+
 import { Link, Wrapper, Section } from '@/components'
 import styles from './Masthead.module.scss'
 import { IMasthead } from './Masthead.types'
+import { useScramble } from 'use-scramble'
+import { useEffect } from 'react'
 
-export async function Masthead({ heading, subheading, children }: IMasthead) {
+export function Masthead({ heading, subheading, children }: IMasthead) {
+  const { ref, replay } = useScramble({
+    text: subheading,
+    overdrive: false,
+    overflow: true,
+    range: [97, 122],
+    chance: 0.5,
+    seed: 10,
+    playOnMount: false,
+  })
+
+  useEffect(() => replay(), [replay])
+
   return (
     <Section id="home" noPadding>
       <div className={styles.container}>
         <Wrapper size="xl">
           <h1 className={styles.heading}>{heading}</h1>
-          <p className={styles.subheading}>{subheading}</p>
+          <p className={styles.subheading} ref={ref} />
           <Link variant="border" href="/cv" target="_blank">
             Curriculum Vitae
           </Link>
